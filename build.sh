635aaebe45f7dfa5914840564def6e0f23ebf523
Wil <wilmerulcuevelasco@gmail.com>
2025-07-22 13:16:25 -0500

    Actualizados yaml y build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # Instala dependencias para WeasyPrint
 apt-get update
 apt-get install -y build-essential python3-dev python3-pip python3-setuptools python3-wheel python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info fonts-liberation
@@ -6,8 +7,5 @@ apt-get install -y build-essential python3-dev python3-pip python3-setuptools py
 # Instala dependencias de Python
 pip install -r requirements.txt
 
-# Cambiar el directorio correcto
-cd ProyectoSCSM_django/ProyectoSCSM
-
-# Recoge archivos estáticos
+# Recoge archivos estáticos desde la raíz del proyecto
 python manage.py collectstatic --noinput
